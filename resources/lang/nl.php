92b6cc17d7876004880c25a4b1a06466bb4299c6
JustCarmen <carmen@justcarmen.nl>
2023-01-08 19:55:00 +0100

    Add dutch translation

---
+++ resources/lang/nl.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Simple menu module' => 'Simpele menu module',
+    'Add an extra mainmenu item and page.' => 'Voeg een extra hoofdmenu item en pagina toe.',
+    'Leave the page title and text blank if you only want an additional footer to add your credits or copyright, for example.' =>
+    'Laat de paginatitel en -tekst leeg als u alleen een extra voettekst wilt om bijvoorbeeld uw credits of copyright toe te voegen.',
+    'Page title' => 'Pagina titel',
+    'Page text' => 'Pagina tekst'
+];
