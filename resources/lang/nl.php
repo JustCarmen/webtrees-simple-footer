bff93fea26f47bba7669aa520931980ea7463e87
JustCarmen <carmen@justcarmen.nl>
2024-02-09 23:16:53 +0100

    Fix bug in translation terms

--- resources/lang/nl.php
+++ resources/lang/nl.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'Simple menu module' => 'Simpele menu module',
-    'Add an extra mainmenu item and page.' => 'Voeg een extra hoofdmenu item en pagina toe.',
+    'Simple footer module' => 'Simpele voettekst module',
+    'Add an extra footer and page.' => 'Voeg een extra voettekst en pagina toe.',
     'Leave the page title and text blank if you only want an additional footer to add your credits or copyright, for example.' =>
     'Laat de paginatitel en -tekst leeg als u alleen een extra voettekst wilt om bijvoorbeeld uw credits of copyright toe te voegen.',
     'Page title' => 'Pagina titel',
