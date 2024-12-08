e25933da916bcb5087f4e2f25d5e90259734203d
Gudjon Sigurdsson <100283838+gudjonsi@users.noreply.github.com>
2024-12-08 16:51:41 +0000

    Icelandic language file created - is.php
    Icelandic language file created for webtrees-simple-menu.

---
+++ resources/lang/is.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Simple menu module' => 'Einföld síðufótseining',
+    'Add an extra mainmenu item and page.' => 'Bættu við auka atriði í síðufót og síðu.',
+    'Leave the page title and text blank if you only want an additional footer to add your credits or copyright, for example.' =>
+    'Skildu reitina síðuheiti og -texta eftir auða ef þú vilt aðeins viðbótar síðufót til að bæta við tileinkunn eða höfundarrétti, sem dæmi.',
+    'Page title' => 'Síðuheiti',
+    'Page text' => 'Síðutexti'
+];
